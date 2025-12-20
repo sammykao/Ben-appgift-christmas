f52641e3ca6cfe4ed8d03cc31092234477daa827
Sammy Kao <sammy.kao@tufts.edu>
2025-12-20 01:42:04 -0500

    ios appstore bundle config

--- app.config.ts
+++ app.config.ts
@@ -10,9 +10,24 @@ const config: ExpoConfig = {
   orientation: "portrait",
   scheme: "thementalpitch",
   platforms: ["ios", "android", "web"],
+  icon: "./assets/images/app_logo.png",
   plugins: ["expo-router"],
   experiments: {
     typedRoutes: true
+  },
+  ios: {
+    bundleIdentifier: "com.mentalpitch.app",
+    buildNumber: "1",
+    supportsTablet: true,
+    requireFullScreen: false,
+  },
+  android: {
+    package: "com.mentalpitch.app",
+    versionCode: 1,
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/app_logo.png",
+      backgroundColor: "#ffffff",
+    },
   },
   extra: {
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
@@ -21,5 +36,4 @@ const config: ExpoConfig = {
 };
 
 export default config;
-
 
